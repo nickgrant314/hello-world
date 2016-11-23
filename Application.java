8d6ee43996864cdb7f9107fd09847a9c0d6a0060
nickgrant314 <nickgrant314@gmail.com>
2016-11-23 09:45:32 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myInteger = 65536;
+		short myShort = 32000;
+		long myLong = 70000;
+		
+		char myChar = 'y';
+		boolean myBool = true;
+		
+		double myDouble = 356.6742;
+		float myFloat = 342.3f;		//The 'f' on the end is what truly turns the value from double to float
+		
+		System.out.println(myInteger);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		
+	}
+
+}
